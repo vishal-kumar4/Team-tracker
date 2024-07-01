ca7903cd5794ccb5b0b24ef2331fd8d97f553709
Vishal Kumar <139634737+vishal-beingzero@users.noreply.github.com>
2024-07-01 20:20:03 +0530

    added cors

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 
 app.use(express.json())
 
+
+const cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
+
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
 })
